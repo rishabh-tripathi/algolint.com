799f6d3099b842e2cffd63aefb382ee4d40e86cc
rishabh23 <rishabh23@david-brearley.dreamhost.com>
2014-05-24 02:04:28 -0700

    base checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlgolintCom::Application.config.secret_token = 'e0078f11f50108089850e3103a1dc2a4438e36fe388889177ba6f24848bddd31cee7f2dab46f408a02c57ec94cac6d1c5f0756e455084e28dde8c71a2057e718'
