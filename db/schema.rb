ae5a3df47489f3844c182a970c44024a05b3f340
rishabh23 <rishabh23@david-brearley.dreamhost.com>
2014-06-13 04:31:19 -0700

    server fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140602164521) do
+ActiveRecord::Schema.define(:version => 20140603045819) do
 
   create_table "contents", :force => true do |t|
     t.integer  "user_id"
@@ -21,8 +21,10 @@ ActiveRecord::Schema.define(:version => 20140602164521) do
     t.integer  "compile"
     t.integer  "status"
     t.integer  "sharability"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+    t.integer  "template"
+    t.integer  "template_cat"
   end
 
   create_table "users", :force => true do |t|
